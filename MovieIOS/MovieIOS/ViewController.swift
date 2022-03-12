3c714dcf52014ddddeca1057d2f0efe0ad6d4c4b
Tifo Audi Alif Putra <tifoaudi@ruangguru.com>
2022-03-12 14:08:10 +0700

    init project

---
+++ MovieIOS/MovieIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieIOS
+//
+//  Created by Tifo Audi Alif Putra on 12/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
